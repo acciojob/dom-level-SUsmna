7c45acf0834cf16ba60786afc0cd53ba7979d2a1
SUsmna <159513831+SUsmna@users.noreply.github.com>
2024-06-06 22:01:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your JS code here. If required.
+function findDOMLevel(){
+	const element = document.getElementById('level');
+
+	let level = 0;
+
+	let currentElement = element;
+	while (currentElement) {
+		level++;
+		currentElement = currentElement.parentElement;	
+	}
+	alert(`The level of the element is: ${level}`);
+}
+findDOMLevel()
